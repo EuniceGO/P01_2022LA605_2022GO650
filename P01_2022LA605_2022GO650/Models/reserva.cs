098392781319f2d2f622c7d42ffb0209f8f9c5d4
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-02-24 17:26:54 -0600

    Agregando modelo usuario, reservas

---
+++ P01_2022LA605_2022GO650/Models/reserva.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022LA605_2022GO650.Models
+{
+    public class reserva
+    {
+        [Key]
+        public int reserva_id {  get; set; }
+        public int usuario_id {  get; set; }
+    }
+}
---
+++ P01_2022LA605_2022GO650/Models/usuario.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022LA605_2022GO650.Models
+{
+    public class usuario
+    {
+        [Key]
+        public int usuario_id {  get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono {  get; set; }
+        public string contraseña {  get; set; }
+        public string rol {  get; set; }
+
+    }
+}
