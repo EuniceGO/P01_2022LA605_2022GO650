dca69349f4d3ae9bd23f03bdf7db7ec1eb7fcb68
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-02-24 17:19:04 -0600

    Agregando inyeccion

---
+++ P01_2022LA605_2022GO650/Models/ParqueoContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_2022LA605_2022GO650.Models
+{
+    public class ParqueoContext : DbContext
+    {
+        protected ParqueoContext()
+        {
+        }
+    }
+}
--- P01_2022LA605_2022GO650/Program.cs
+++ P01_2022LA605_2022GO650/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using P01_2022LA605_2022GO650.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ParqueoContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
