07f24e1cf074fd5395dfaa4e136aecb307d359f3
kleal <kathya.leal@catolica.edu.sv>
2025-02-24 17:28:43 -0600

    Modelos Sucursal y espacioParqueo

---
+++ P01_2022LA605_2022GO650/Models/Sucursal.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022LA605_2022GO650.Models
+{
+    public class Sucursal
+    {
+        [Key]
+        public int SucursalId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Direccion { get; set; }
+
+        [MaxLength(20)]
+        public string Telefono { get; set; }
+
+        public int? AdministradorId { get; set; } // Puede ser null si aún no tiene admin
+
+        public int NumEspacios { get; set; }
+
+        // Relación con Usuarios (Administrador)
+        [ForeignKey("AdministradorId")]
+        public usuario Administrador { get; set; }
+
+        // Relación con Espacios de Parqueo
+        public ICollection<espacioParqueo> EspaciosParqueo { get; set; }
+
+
+
+    }
+}
---
+++ P01_2022LA605_2022GO650/Models/espacioParqueo.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022LA605_2022GO650.Models
+{
+    public class espacioParqueo
+    {
+        public class EspacioParqueo
+        {
+            [Key]
+            public int EspacioId { get; set; }
+
+            [Required]
+            public int SucursalId { get; set; }
+
+            [Required]
+            [MaxLength(10)]
+            public string NumeroEspacio { get; set; }
+
+            [MaxLength(50)]
+            public string Ubicacion { get; set; }
+
+            [Required]
+            public decimal CostoHora { get; set; }
+
+            [Required]
+            public string Estado { get; set; } = "disponible"; // Disponible por defecto
+
+            // Relación con Sucursal
+            [ForeignKey("SucursalId")]
+            public Sucursal Sucursal { get; set; }
+        }
+    }
+}
