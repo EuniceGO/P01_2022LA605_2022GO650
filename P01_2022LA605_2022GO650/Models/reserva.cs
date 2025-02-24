c491ae8cd79255b53f6990403585c997ceb5751b
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-02-24 17:30:20 -0600

    Agregando cambios de Reserva

--- P01_2022LA605_2022GO650/Models/reserva.cs
+++ P01_2022LA605_2022GO650/Models/reserva.cs
@@ -5,7 +5,13 @@ namespace P01_2022LA605_2022GO650.Models
     public class reserva
     {
         [Key]
-        public int reserva_id {  get; set; }
-        public int usuario_id {  get; set; }
+        public int reserva_id { get; set; }
+        public int usuario_id { get; set; }
+        public int espacio_id { get; set; }
+        public DateTime fecha_reserva {  get; set; }
+        public int cantidad_horas { get; set; }
+        
+        public string estado {  get; set; }
+
     }
 }
