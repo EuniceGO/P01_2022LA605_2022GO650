804ecfbec52154803ca0e145a474cb104b1c89ca
kleal <kathya.leal@catolica.edu.sv>
2025-02-24 17:31:41 -0600

    db context

--- P01_2022LA605_2022GO650/Models/ParqueoContext.cs
+++ P01_2022LA605_2022GO650/Models/ParqueoContext.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using static P01_2022LA605_2022GO650.Models.espacioParqueo;
 
 namespace P01_2022LA605_2022GO650.Models
 {
-    public class ParqueoContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
-        protected ParqueoContext()
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<usuario> Usuarios { get; set; }
+        public DbSet<Sucursal> Sucursales { get; set; }
+        public DbSet<espacioParqueo> EspaciosParqueo { get; set; }
+        public DbSet<reserva> Reservas { get; set; }
     }
 }
